e4b92d5fb3e36d767a4573f3bfaee2e19e4cf4fc
Hammer Ádám <hammer.adam@thisisdone.com>
2020-09-12 23:46:26 +0200

    kinda ready

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const CELL_WIDTH = 10;
+    const INTERVAL_SPEED = 10;
+
     const board = document.querySelector('.board');
-    const width = Math.floor(board.offsetWidth / 20);
-    const height = Math.floor(board.offsetHeight / 20);
+    const width = Math.floor(board.offsetWidth / CELL_WIDTH);
+    const height = Math.floor(board.offsetHeight / CELL_WIDTH);
     const startButton = document.querySelector('.start_btn');
     let gameInterval;
     let currentCellArray = [];
 
-    board.style.width = `${width * 20}px`;
-    board.style.height = `${height * 20}px`;
+    board.style.width = `${width * CELL_WIDTH}px`;
+    board.style.height = `${height * CELL_WIDTH}px`;
+    board.parentElement.style.display = 'flex';
+    board.parentElement.style.alignItems = 'center';
+    board.parentElement.style.justifyContent = 'center';
+    board.parentElement.style.flexDirection = 'column';
 
     // Create the cells
     for (let i = 0; i < height; i++) {
@@ -53,51 +60,106 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const startGame = () => {
         gameInterval = setInterval(() => {
-            console.log('round');
             const newCellArray = [];
-
             currentCellArray.forEach(row => {
                 newCellArray.push([...row]);
-            })
-
-            cells.forEach(cell => {
-                const neighbourCount = getNumberOfNeighbours(cell);
-                const isAlive = cell.classList.contains('active');
-
-                if (isAlive) {
-
-                    return;
-                }
-
             });
-        }, 1000);
+
+            currentCellArray.forEach((row, rowNumber) => {
+                row.forEach((isAlive, colNumber) => {
+
+                    const neighbourCount = getNumberOfNeighbours(rowNumber, colNumber);
+                    if (isAlive) {
+                        if (neighbourCount === 2 || neighbourCount === 3) {
+                            newCellArray[rowNumber][colNumber] = 1;
+                            return;
+                        }
+
+                        newCellArray[rowNumber][colNumber] = 0;
+                        return;
+                    }
+
+                    if (neighbourCount === 3) {
+                        newCellArray[rowNumber][colNumber] = 1;
+                    }
+                });
+            });
+
+            currentCellArray = newCellArray;
+
+            document.querySelectorAll('.board__row').forEach((row, rowNumber) => {
+               row.querySelectorAll('.board__row__cell').forEach((cell, colNumber) => {
+                  if (currentCellArray[rowNumber][colNumber]) {
+                      cell.classList.add('active');
+
+                      return;
+                  }
+
+                  cell.classList.remove('active');
+               });
+            });
+        }, INTERVAL_SPEED);
     }
 
-    const getNumberOfNeighbours = cell => {
-        const row = parseInt(cell.getAttribute('row'),10);
-        const col = parseInt(cell.getAttribute('col'), 10);
-        const neighbourCount = 0;
+    const getNumberOfNeighbours = (row, col) => {
+        let neighbourCount = 0;
 
-        // if (row !== 0) {
-        //     // Top left
-        //     if (col !== 0) {
-        //         const topLeft = document.querySelector(`[data-row="${row - 1}"][data-row="${col - 1}"]`)
-        //         console.log(`[data-row="${row - 1}"][data-row="${col - 1}"]`);
-        //         console.log(topLeft.classList.contains('active'));
-        //     }
-        // }
-        //
-        // if (row !== width - 1) {
-        //
-        // }
-        //
-        // if (col !== 0) {
-        //
-        // }
-        //
-        // if (col !== height - 1) {
-        //
-        // }
+        if (row !== 0) {
+            // Top left
+            if (col !== 0) {
+                if (currentCellArray[row - 1][col - 1] === 1) {
+                    neighbourCount++;
+                }
+            }
+
+            // Top
+            if (currentCellArray[row - 1][col] === 1) {
+                neighbourCount++;
+            }
+
+            // Top right
+            if (col !== width - 1) {
+                if (currentCellArray[row - 1][col + 1] === 1) {
+                    neighbourCount++;
+                }
+            }
+        }
+
+        if (row !== height - 1) {
+            // Bottom left
+            if (col !== 0) {
+                if (currentCellArray[row + 1][col - 1] === 1) {
+                    neighbourCount++;
+                }
+            }
+
+            // Bottom
+            if (currentCellArray[row + 1][col] === 1) {
+                neighbourCount++;
+            }
+
+            // Bottom right
+            if (col !== width - 1) {
+                if (currentCellArray[row + 1][col + 1] === 1) {
+                    neighbourCount++;
+                }
+            }
+        }
+
+        // Left
+        if (col !== 0) {
+            if (currentCellArray[row][col - 1] === 1) {
+                neighbourCount++;
+            }
+        }
+
+        // Right
+        if (col !== height - 1) {
+            if (currentCellArray[row][col + 1] === 1) {
+                neighbourCount++;
+            }
+        }
+        return neighbourCount;
     }
 
     const stopGame = () => {
